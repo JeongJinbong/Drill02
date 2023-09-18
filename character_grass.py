c577d73b12da0c8afb1eaf98086a9b1748f0decd
JeongJinbong(NoteBook) <jbong0104@naver.com>
2023-09-18 12:23:09 +0900

    Drill2 제출(2020180036 정진봉)

--- character_grass.py
+++ character_grass.py
@@ -8,36 +8,37 @@ character = load_image('character.png')
 
 x= 0
 y=90
-while(1):
- if(y==90 and x==0):
-     while (x<800):
+if(y==90 and x==0):
+    while (x<800):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         x= x+2
         delay(0.01)
- if(y== 90 and x==800):
-     while(y<600):
+if(y== 90 and x==800):
+    while(y<600):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         y= y+2
         delay(0.01)
- if(y== 600 and x==800):
-     while(x>0):
+if(y== 600 and x==800):
+    while(x>0):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         x= x-2
         delay(0.01)
- if(y== 600 and x== 0):
-     while(y>90):
+if(y== 600 and x== 0):
+    while(y>90):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         y=y-2
         delay(0.01)
-
+if (y==90 and x==0)
+    while(x
+        
 
      
     
