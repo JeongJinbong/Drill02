3ed37cc8b85efc7b54092d919a2c0ff5e8c568ef
JeongJinbong(NoteBook) <jbong0104@naver.com>
2023-09-18 13:43:51 +0900

    Drill2 제출(2020180036 정진봉)

--- drill02.py
+++ drill02.py
@@ -6,37 +6,59 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+a= True
+
 x= 0
 y=90
+r=200
+theta = 0
 while(1):
- if(y==90 and x==0):
+ if(a):   
+  if(y==90 and x==0):
      while (x<800):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         x= x+2
         delay(0.01)
- if(y== 90 and x==800):
+  if(y== 90 and x==800):
      while(y<600):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         y= y+2
         delay(0.01)
- if(y== 600 and x==800):
+  if(y== 600 and x==800):
      while(x>0):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         x= x-2
         delay(0.01)
- if(y== 600 and x== 0):
+  if(y== 600 and x== 0):
      while(y>90):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         y=y-2
         delay(0.01)
+        a= False
+  if(a ==False):
+     x= 300
+     y= 200
+     for theta in range (0,5):
+         clear_canvas_now()
+         grass.draw_now(400,30)
+         x= x+r* math.cos(theta)
+         y= y+r * math.sin(theta)
+         character.draw_now(x,y)
+         delay(0.1)
+         if(theta==5-1):
+           a=True
+           x=0
+           y=90
+
+    
 
 
      
